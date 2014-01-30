9375d9dae85a7411f4c4423c3ec30df61eb91a4e
kenklin <github.kenklin@kenlin.com>
2014-01-30 12:52:47 -0500

    Apache License, Version 2.0

--- src/main/java/com/kenlin/awsec2offering/Offering.java
+++ src/main/java/com/kenlin/awsec2offering/Offering.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 Ken K. Lin. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.kenlin.awsec2offering;
 
 import java.io.IOException;
@@ -10,6 +25,15 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+/**
+ * @todo Revisit the JSON schema by comparing the current output to
+ * aws ec2 describe-reserved-instances-offerings
+ *		--no-include-marketplace
+ *		--availability-zone us-east-1a
+ *		--product-description "Linux/UNIX"
+ *		--offering-type "heavy utilization"
+ *		--instance-type t1.micro
+ */
 @JsonAutoDetect
 public class Offering {
 	public final static String	AVAILABILITYZONE	= "availabilityZone";
